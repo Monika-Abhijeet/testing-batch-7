25e016729bcb6576fc55ef1a09fbdc96ae8cd381
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-06-20 15:23:57 +0530

    document.write and window.print

--- learn-javascript.js
+++ learn-javascript.js
@@ -1,6 +1,12 @@
-console.log("hello");
 function changeName() {
+  //   alert("HI");
+  //   console.log("hello");
+
   document.getElementById("text").innerHTML = "Monika Abhijeet";
   document.getElementById("text").style.fontSize = "50px";
   document.getElementById("text").style.color = "blue";
+}
+
+function replaceData() {
+  document.write("heyyy i ahve replaced entire screen");
 }
