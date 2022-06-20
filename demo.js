26b9c8f4541e7d0cfcb9fb252a284d36df69be48
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-06-20 15:18:26 +0530

    document.getElementByID

--- demo.js
+++ demo.js
@@ -1,5 +1,3 @@
 alert("Hi Everyone");
 console.log("Welcome to javascript class");
 console.log("hope you are liking it");
-
-console.log(5 + 6);
---
+++ learn-javascript.js
@@ -0,0 +1,6 @@
+console.log("hello");
+function changeName() {
+  document.getElementById("text").innerHTML = "Monika Abhijeet";
+  document.getElementById("text").style.fontSize = "50px";
+  document.getElementById("text").style.color = "blue";
+}
