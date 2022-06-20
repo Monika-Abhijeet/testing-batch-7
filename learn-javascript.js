18af115d86fdf651b96af8caf6ab3bd70fb5a7ba
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-06-20 17:38:36 +0530

    control structures

--- learn-javascript.js
+++ learn-javascript.js
@@ -39,3 +39,63 @@ function divide() {
 
   document.getElementById("result").innerHTML = result;
 }
+// array of string
+let alphabets = ["A", "B", "C", "D", "E"];
+console.log(alphabets[4]);
+
+let employee = {
+  name: "monika",
+  age: 28,
+  phno: 12324,
+  email: "abc@gmail.com",
+};
+console.log(employee.phno);
+
+// var num = prompt("Enter number");
+// if (num > 0) {
+//   alert("given number is positive");
+// } else {
+//   alert("given number is not positive");
+// }
+// var password = prompt("enter password");
+// if (password.length === 0) {
+//   alert("pls enter password");
+// } else if (password.length < 5) {
+//   alert("week password");
+// } else if (password.length > 12) {
+//   alert("password too long");
+// } else {
+//   alert("good password");
+// }
+
+// let day = prompt("Enter a number between 1 to 7");
+// switch (day) {
+//   case "1":
+//     alert("Sunday");
+//     break;
+//   case "2":
+//     alert("Monday");
+//     break;
+//   case "3":
+//     alert("Tuesday");
+//     break;
+//   case "4":
+//     alert("Wednesday");
+//     break;
+//   case "5":
+//     alert("Thursday");
+//     break;
+//   case "6":
+//     alert("Friday");
+//     break;
+//   case "7":
+//     alert("Saturday");
+//     break;
+//   default:
+//     alert("not a week day");
+//     break;
+// }
+
+for (i = 10; i <= 20; i++) {
+  console.log(i * 5);
+}
