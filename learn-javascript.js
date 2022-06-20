4f073ed5f202908284c49eedbffd9b5f619edfa0
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-06-20 15:57:58 +0530

    add and subtract functionality

--- learn-javascript.js
+++ learn-javascript.js
@@ -1,7 +1,4 @@
 function changeName() {
-  //   alert("HI");
-  //   console.log("hello");
-
   document.getElementById("text").innerHTML = "Monika Abhijeet";
   document.getElementById("text").style.fontSize = "50px";
   document.getElementById("text").style.color = "blue";
@@ -9,4 +6,20 @@ function changeName() {
 
 function replaceData() {
   document.write("heyyy i ahve replaced entire screen");
+}
+
+function add() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var result = parseInt(a) + parseInt(b);
+
+  document.getElementById("result").innerHTML = result;
+}
+
+function subtract() {
+  var a = document.getElementById("num1").value;
+  var b = document.getElementById("num2").value;
+  var result = parseInt(a) - parseInt(b);
+
+  document.getElementById("result").innerHTML = result;
 }
